8a4f9b98514f63330f8ad688a0d3abaab81642e8
mariamsamaha <mariamtoty.98306284@gmail.com>
2026-02-02 02:39:20 +0200

    Initial MVP DB schema and model files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "eg.edu.zewailcity.bloodconnect"
+    namespace = "com.bloodconnect.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "eg.edu.zewailcity.bloodconnect"
+        applicationId = "com.bloodconnect.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
